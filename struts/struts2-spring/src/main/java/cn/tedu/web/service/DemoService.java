1eb7c9d79ae87079f54c09162660f9eafbb976ce
zhangly <zhanglylx@163.com>
2020-06-07 23:19:24 +0800

    excel依赖

--- struts/struts2-spring/src/main/java/cn/tedu/web/service/DemoService.java
+++ struts/struts2-spring/src/main/java/cn/tedu/web/service/DemoService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class DemoService {
     public String test() {
+        System.out.println(Thread.currentThread().getId());
         return "Hello World!";
     }
 }

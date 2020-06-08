28899f99641637730cb986c21f42de8d95ab9659
zhangly <zhanglylx@163.com>
2020-06-08 11:05:00 +0800

    引入struts2拦截器，用于获取参数

--- struts/result/src/main/java/cn/tedu/web/DemoAction.java
+++ struts/result/src/main/java/cn/tedu/web/DemoAction.java
@@ -6,8 +6,20 @@ import org.springframework.stereotype.Controller;
 @Controller
 @Scope("prototype")
 public class DemoAction {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String execute() {
+        System.out.println(name);
         System.out.println("DemoAction");
+
         return "success";
     }
 }
